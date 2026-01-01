9c753f3a5a4219207249fa56531eb09d4f77cd66
kdp-narola <kdp@narola.email>
2026-01-01 16:28:41 +0530

    Fix- small change

--- routes/booking.routes.js
+++ routes/booking.routes.js
@@ -2,7 +2,7 @@ const ownerBookingDetails = require("../controllers/reservation/bookingHistory")
 const asyncHandler = require("../utils/asyncHandler");
 const router = require("express").Router();
 
-router.post('/', asyncHandler(async function _Bookings(req, res, next) {
+router.post('/', asyncHandler(async function _ownerBookingDetails(req, res, next) {
     const data = await ownerBookingDetails(req?.authUser, req.body);
     res.ok(data);
 }));
